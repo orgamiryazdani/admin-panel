a7f7b99a41f5c503838221507646124dbc330a5d
amir <orgamiryazdani@gmail.com>
2023-11-19 22:54:48 +0330

    responsive course list

--- src/features/courses/components/course-list.jsx
+++ src/features/courses/components/course-list.jsx
@@ -1,19 +1,17 @@
 import Course from "./course";
 
-const CourseList = ({ courses }) => {
-    return (
-        <>
-            <div className="row">
-                {
-                    courses.map((course) => (
-                        <div key={course.id} className="col-3">
-                            <Course {...course} />
-                        </div>
-                    ))
-                }
-            </div>
-        </>
-    );
-}
+const CourseList = ({courses}) => {
 
+  return (
+    <>
+      <div className="row">
+        {courses.map((course) => (
+          <div className="col-12 col-md-6 col-lg-3" key={course.id}>
+            <Course {...course} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 export default CourseList;
